2f38f8882a8cdb1239e9e772a1c34eb6237de73d
Larry Walters <lwalters@gannett.com>
2020-04-22 13:28:44 -0400

    Some cleanup

--- update_wordpress_pass.py
+++ update_wordpress_pass.py
@@ -1,9 +1,18 @@
-#!/usr/bin/python
-#import json
-import os
+#!/usr/bin/env python3
+
+### The script updates the admin password for a wordpress site
+### Hosted in a GKE environments
+### Uses python3, Pick and Kubernetes packages are required
+### To use chmod u+x name.py; ./name.py
+### Created by the larwal6312
+
 import random
 import string
 import sys
+import subprocess
+print ("Verifying Dependencies")
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pick', '--user'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'kubernetes', '--user'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 from kubernetes import client, config, watch
 from kubernetes.client import Configuration
 from kubernetes.client.apis import core_v1_api
@@ -11,6 +20,7 @@ from kubernetes.client.rest import ApiException
 from kubernetes.stream import stream
 from pick import pick
 
+# Finds pods deployed in namespace
 def find_pods():
     pods = api.list_namespaced_pod(
             namespace=ns, watch=False)
@@ -21,20 +31,23 @@ def find_pods():
             wordpress_pods.append(name)
     return wordpress_pods
 
+# Uses pick to list and select pod
 def choose_pod(wordpress_pods):
     option, _ = pick(wordpress_pods, title="Choose the pod")
-    print("%s Selected" %(option))
     pod = option
     return pod
 
+# Generates random password
 def generate_password(stringLength=20):
     print ("Generating new password")
     letters_numbers = string.ascii_letters + string.digits
     return ''.join((random.choice(letters_numbers) for i in range(stringLength)))
     print ("New password created")
 
+# Issues command to pod to update wp password
 def update_password(pod, password):
     print ("Updating gdadmin user\'s password on %s" %(pod))
+    print ("This may take some time")
     exec_command = [
         "/bin/sh",
         "-c",
@@ -43,12 +56,13 @@ def update_password(pod, password):
                     pod,
                     ns,
                     command=exec_command,
-                    stderr=True,
+                    stderr=False,
                     stdin=False,
                     stdout=True,
                     tty=False)
     print(update)
 
+# Doing all the things
 def main():
     wordpress_pods = find_pods()
     pod = choose_pod(wordpress_pods)
@@ -60,6 +74,8 @@ def main():
     print ("password: %s" %(password))
 
 if __name__ == '__main__':
+
+    #Reads in kube config file and sets up namespace
     contexts, active_context = config.list_kube_config_contexts()
     if not contexts:
         print("Cannot find any context in kube-config file.")
@@ -71,7 +87,6 @@ if __name__ == '__main__':
     print("%s Selected" %(option))
 
     ns = option
-    os.system("kubectx %s" %(ns))
     config.load_kube_config()
     api = client.CoreV1Api()
 
